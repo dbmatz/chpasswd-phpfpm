2802e18bd32e36c54a1fe29659aa2f367c44e464
dbmatz <matzenbacherdebora@gmail.com>
2023-05-04 15:48:43 -0400

    teste

--- src/chpasswd.php
+++ src/chpasswd.php
@@ -145,8 +145,8 @@
     fwrite($open, $mensagem);
     fclose($open);
 
-    file_put_contents("php://stderr", "$mensagem\n");
-    file_put_contents("php://stdout", "$mensagem\n");
+    /*file_put_contents("php://stderr", "$mensagem\n");
+    file_put_contents("php://stdout", "$mensagem\n");*/
 
     return 0;
   }
